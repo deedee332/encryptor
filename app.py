99f54a3db4b14624aa8c3c13ec3290556d999b65
Eluwoyin Darasimi <eluwoyindarasimi2@gmail.comgit config --global --listgit config --global user.name deedee332git config --global user.email eluwoyindarasimi2@gmail.com>
2025-02-25 20:04:54 +0100

    Fix: Bind Flask to 0.0.0.0 and Render's PORT

--- app.py
+++ app.py
@@ -95,3 +95,8 @@ def decrypt_file_route():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use Render's assigned PORT or default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
